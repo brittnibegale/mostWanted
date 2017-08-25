77888453df061e3cf40931525c034faa433df918
ravenkostrzewa <ravekostrzewa@gmail.com>
2017-08-25 13:27:36 -0500

    fixed spacing

--- mostWanted.js
+++ mostWanted.js
@@ -26,8 +26,7 @@ var informationForSearch
 var userInput = informationForSearch;
 userInput = prompt ("Would you like to search by eye color? Enter 'yes' or 'no.'").toLowerCase();
 	if (userInput !== "yes" && userInput !== "no") {
-		userInput = prompt ("Do you k
-		now the eye color of the person for whom you are seaching? Enter 'yes' or 'no'.").toLowerCase();
+		userInput = prompt ("Do you know the eye color of the person for whom you are seaching? Enter 'yes' or 'no'.").toLowerCase();
 	}
 	else if (userInput === "yes") {
 		var eyeColor = prompt ("Are the person's eyes black, blue, brown, green, or hazel?").toLowerCase(); 
